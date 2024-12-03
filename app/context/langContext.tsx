18dff0aa737c786a41d6a95f63513fd9261038ee
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-12-03 14:29:43 +0100

    :art: done with the context  :art:

--- app/context/langContext.tsx
+++ app/context/langContext.tsx
@@ -3,3 +3,24 @@ import { createContext, useContext, useState } from "react";
 import {LangContextType} from "@/app/types/types"
 
 const LangContext = createContext<LangContextType | undefined>(undefined);
+
+export function LangProvider({children, defaultLang = "fr"}:{children: React.ReactNode; defaultLang?: string} ){
+    const [lang, setLang] = useState(defaultLang)
+
+    return (
+        <LangContext.Provider value={{lang, setLang}}>
+            {children}
+        </LangContext.Provider>
+    )
+
+}
+
+export function useLang() {
+    const context =useContext(LangContext)
+  
+    if (context === undefined){
+      throw new Error ("useLang must be used within a LangProvider")
+    }
+    return context
+  }
+  
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { LangProvider } from "./context/langContext";
+import { useContext } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <LangProvider>
         {children}
+        </LangProvider>
       </body>
     </html>
   );
